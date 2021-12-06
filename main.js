857800bc6fef523993630ea9bfe7052da3e499fd
Topi <84213227+apple-pi-yhhhh@users.noreply.github.com>
2021-12-07 08:03:54 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,47 +1,39 @@
+const { app, BrowserWindow } = require('electron')
 
-// アプリケーション作成用のモジュールを読み込み
-const {app, BrowserWindow} = require('electron');
-
-// メインウィンドウ
-let mainWindow;
+let win
 
 function createWindow() {
-  // メインウィンドウを作成します
-  mainWindow = new BrowserWindow({
-    webPreferences: {
-      nodeIntegration: true,
-      contextIsolation: false
-    },
-    width: 800, height: 600,
-  });
 
-  // メインウィンドウに表示するURLを指定します
-  // （今回はmain.jsと同じディレクトリのindex.html）
-  mainWindow.loadFile('index.html');
+    //ウインドウの作成
+    win = new BrowserWindow({ width: 800, height: 400 })
 
-  // デベロッパーツールの起動
-  mainWindow.webContents.openDevTools();
+    //ウインドウに表示する内容
+    win.loadFile('index.html')
 
-  // メインウィンドウが閉じられたときの処理
-  mainWindow.on('closed', () => {
-    mainWindow = null;
-  });
+    //デバッグ画面表示
+    // win.webContents.openDevTools()
+
+    //このウインドウが閉じられたときの処理
+    win.on('closed', () => {
+        win = null
+    })
 }
 
-//  初期化が完了した時の処理
-app.on('ready', createWindow);
+//アプリが初期化されたとき（起動されたとき）
+app.on('ready', () => {
+    createWindow()
+})
 
-// 全てのウィンドウが閉じたときの処理
+//全ウインドウが閉じられたとき
 app.on('window-all-closed', () => {
-  // macOSのとき以外はアプリケーションを終了させます
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
-});
-// アプリケーションがアクティブになった時の処理(Macだと、Dockがクリックされた時）
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
+
+//アクティブになったとき（MacだとDockがクリックされたとき）
 app.on('activate', () => {
-  // メインウィンドウが消えている場合は再度メインウィンドウを作成する
-  if (mainWindow === null) {
-    createWindow();
-  }
-});
+    if (win === null) {
+        createWindow()
+    }
+})
